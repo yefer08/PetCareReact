99ef4638e2d1c473c564dab9d008c83daa3fc60e
KarinaUribe <nannisuribe@gmail.com>
2026-03-28 15:45:11 -0500

    Se crea app como principal

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import Navigation from './src/navigation/Navigation';
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>PetCare React is running</Text>
-    </SafeAreaView>
-  );
-}
+const App = () => {
+  return <Navigation />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 22,
-    fontWeight: '600',
-    color: '#000',
-  },
-});
+export default App;
