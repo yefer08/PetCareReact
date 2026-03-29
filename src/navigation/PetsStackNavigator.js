94a766a3b9b2b7d13621ad8fce54327182c0ab51
KarinaUribe <nannisuribe@gmail.com>
2026-03-28 22:44:47 -0500

    Adjustments are made to the colors of the tab bar, to the most prominent active tabs, and emoji icons are added without installing anything extra.

--- src/navigation/PetsStackNavigator.js
+++ src/navigation/PetsStackNavigator.js
@@ -2,14 +2,39 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PetListScreen from '../screens/PetListScreen';
 import PetDetailScreen from '../screens/PetDetailScreen';
+import { COLORS } from '../constants/colors';
 
 const Stack = createNativeStackNavigator();
 
 const PetsStackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="PetList">
-      <Stack.Screen name="PetList" component={PetListScreen} options={{ title: 'Mascotas' }} />
-      <Stack.Screen name="PetDetail" component={PetDetailScreen} options={{ title: 'Detalle' }} />
+    <Stack.Navigator
+      initialRouteName="PetList"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.white,
+        },
+        headerShadowVisible: false,
+        headerTintColor: COLORS.text,
+        headerTitleStyle: {
+          fontWeight: '800',
+          fontSize: 20,
+        },
+        contentStyle: {
+          backgroundColor: COLORS.background,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="PetList"
+        component={PetListScreen}
+        options={{ title: 'Mascotas' }}
+      />
+      <Stack.Screen
+        name="PetDetail"
+        component={PetDetailScreen}
+        options={{ title: 'Detalle' }}
+      />
     </Stack.Navigator>
   );
 };
